ca17583e448c2188c756ee51138626b6fc74302b
Jefferson Nguyen <98124001+Jefferson-Nguyen@users.noreply.github.com>
2023-12-04 23:16:52 -0500

    ActivityLifeCycleCallbacks

--- app/src/main/java/com/photos/Photos.java
+++ app/src/main/java/com/photos/Photos.java
@@ -1,6 +1,11 @@
 package com.photos;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.photos.database.AlbumsDatabase;
 import com.photos.models.Album;
@@ -11,14 +16,59 @@ public class Photos extends Application {
 
     private static List<Album> albumList;
 
+    private final AlbumsDatabase DB = AlbumsDatabase.getInstance(this);
+
     @Override
     public void onCreate() {
         super.onCreate();
-        AlbumsDatabase albumsDatabase = AlbumsDatabase.getInstance(this);
-        albumList = albumsDatabase.albumDao().getAllAlbums();
+
+        albumList = DB.albumDao().getAllAlbums();
+
+        registerCallbacks();
     }
 
     public static List<Album> getAlbumList() {
         return albumList;
+    }
+
+    private void registerCallbacks() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+                // Perhaps we want to do batch updates but for now no, we will use incremental
+                // DB.albumDao().upsertAlbumList(albumList);
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
     }
 }
